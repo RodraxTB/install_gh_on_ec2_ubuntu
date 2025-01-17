b82b47e01670f44275ca664d506bc6f14ee69e0d
Ubuntu <ubuntu@ip-172-31-23-127.ec2.internal>
2025-01-17 17:36:48 +0000

    Modificaciones en el script para verificación de root y estructuras condicionales

--- installgh.sh
+++ installgh.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
-(type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
-	&& sudo mkdir -p -m 755 /etc/apt/keyrings \
-        && out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \
-        && cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
-	&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
-	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+apt-get update || { echo "Fallo al actualizar los paquetes"; exit 1; }
+apt-get install -y git && echo "Git instalado correctamente"
 
-sudo apt update
-sudo apt install gh
+#Actualizar los paquetes
+	if apt-get update; then
+		echo "Paquetes actualizados correctamente"
+	else
+		echo "Fallo al actualizar los paquetes"
+		exit 1
+	fi
+
+#Instalar git
+	if apt-get install -y git; then
+		echo "Git instalado correctamente"
+	else
+		echo "Fallo en la instalación del Git"
+		exit 1
+	fi
